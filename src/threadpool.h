3e62aab022f85b05561fafd47e5a4e5c1793d55e
planet <priv.planet@gmail.com>
2011-03-25 04:28:12 +0900

    3/25

--- src/threadpool.h
+++ src/threadpool.h
@@ -10,6 +10,7 @@
 #include "mutexlock.h"
 #include "criticalscope.h"
 #include "signalevent.h"
+#include "exception.h"
 
 namespace g2
 	{
@@ -17,7 +18,10 @@ namespace g2
 	class IThreadPool
 		{
 		public:
-			virtual bool Add( void *args, pthread_t *pthreadId ) = 0;
+			virtual pthread_t Add( void *args ) = 0;
+			virtual void Remove( pthread_t tid ) = 0;
+			virtual void Join() = 0;
+			virtual void Stop() = 0;
 		};
 
 	//-----------------------------------------------------------------------------------------//
@@ -30,9 +34,9 @@ namespace g2
 				 run_( true )
 				{
 				}
-			
-			void Stop(){ __sync_bool_compare_and_swap( &run_, false, true ); }
-			bool IsRunnable() const { __sync_fetch_and_and( &run_, true ); }
+
+			void Stop(){ __sync_bool_compare_and_swap( &run_, true, false ); }
+			bool IsRunnable() { __sync_fetch_and_and( &run_, true ); }
 			
 			void* GetArgs() const { return args_; }
 			IThreadPool* GetThreadPool() const { return threadPool_; }
@@ -47,16 +51,24 @@ namespace g2
 	template < class Worker >
 	class ThreadPool :public IThreadPool
 		{
-			G2_MARK_UNCOPYABLE( self_t );
+			G2_MARK_UNCOPYABLE( ThreadPool< Worker > );
 			
 			class PoolableThreading :public Worker
 				{
+					using Worker::SetArgs;
+					
 				public:
 					PoolableThreading( IThreadPool *threadPool, void *args )
 						:args_( threadPool, args )
 						{
 						SetArgs( &args_ );
 						}
+					~PoolableThreading()
+						{
+						puts( "[*] dtor" );
+						}
+
+					void Stop(){ args_.Stop(); }
 					
 				private:
 					WorkerArgs args_;
@@ -70,9 +82,10 @@ namespace g2
 			ThreadPool();
 			~ThreadPool();
 			
-			virtual bool Add( void *args, pthread_t *pthreadId );
-			void Join();
-			void Stop();
+			virtual pthread_t Add( void *args );
+			virtual void Remove( pthread_t tid );
+			virtual void Join();
+			virtual void Stop(){ stopEvent_.Signal(); }
 			
 		private:
 			thread_map_t threads_;
@@ -97,15 +110,35 @@ namespace g2
 
 	//-----------------------------------------------------------------------------------------//
 	template < class Worker >
-	bool ThreadPool< Worker >::Add( void *args, pthread_t *pthreadId )
+	pthread_t ThreadPool< Worker >::Add( void *args )
 		{
 		worker_ptr_t worker( new PoolableThreading( this, args ) );
-		pthread_t newId = worker->Create();
-		if( pthreadId != NULL )
-			*pthreadId = newId;
+		worker->Create();
+		pthread_t tid = worker->GetThreadID();
 		
 		critical_scope_t locked( threadsLock_ );
-		threads_.insert( std::make_pair( newId, worker ) );
+		threads_.insert( std::make_pair( tid, worker ) );
+
+		return tid;
+		}
+
+	//-----------------------------------------------------------------------------------------//
+	template < class Worker >
+	void ThreadPool< Worker >::Remove( pthread_t tid )
+		{
+		critical_scope_t locked( threadsLock_ );
+		if( threads_.empty() )
+			return;
+
+		typename thread_map_t::iterator iter = threads_.find( tid );
+		if( iter->second->GetThreadID() == pthread_self() )
+			{
+			throw Exception( "ThreadPool< Worker >::Remove() can't stop self thread" );
+			}
+		
+		iter->second->Stop();
+		iter->second->Join();
+		threads_.erase( iter );
 		}
 	
 	//-----------------------------------------------------------------------------------------//
@@ -113,10 +146,20 @@ namespace g2
 	void ThreadPool< Worker >::Join()
 		{
 		stopEvent_.Wait();
-		
-		thread_map_t::iterator begin = threads_.begin();
-		thread_map_t::iterator end = threads_.end();
 
-		for(;
+		critical_scope_t locked( threadsLock_ );
+		typename thread_map_t::iterator begin = threads_.begin();
+		typename thread_map_t::iterator end = threads_.end();
+
+		for(; begin != end; ++begin )
+			{
+			begin->second->Stop();
+			}
+
+		begin = threads_.begin();
+		for(; begin != end; ++begin )
+			{
+			begin->second->Join();
+			}
 		}
 	}
