df939cd9a1f6ba5b60fd07f61e1ad6094dbee03b
n <n>
2011-11-11 01:29:34 -0800

    None

--- src/coroutine.h
+++ src/coroutine.h
@@ -4,7 +4,7 @@
 	switch( int &line = l )						\
 	  case -1:									\
 		if(1)									\
-			{puts("out");G2_COROUTINE_OUT:break;}	\
+			{;G2_COROUTINE_OUT:break;}			\
 		else case 0:
 
 #define G2_YIELD														\
@@ -13,4 +13,3 @@
 		for(;;)															\
 			if(line==__LINE__)goto G2_COROUTINE_OUT;					\
 			else if((line=__LINE__)) switch(1)case 1:
-
