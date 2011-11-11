4b6720a0bd69a4ff5e4962f2a8cb8421ed27dd1a
n <n>
2011-11-11 01:17:50 -0800

    coroutine refreshed

--- src/coroutine.h
+++ src/coroutine.h
@@ -1,10 +1,16 @@
 #pragma once
 
-#define CORO(l) switch( int &line = l )						\
-	  case -1:if(1)											\
-			{												\
-            E:												\
-			break;											\
-			} else case 0:
+#define G2_COROUTINE(l)							\
+	switch( int &line = l )						\
+	  case -1:									\
+		if(1)									\
+			{puts("out");G2_COROUTINE_OUT:break;}	\
+		else case 0:
 
-#define YIELD if(0){case __LINE__:;}else for(;;)if(line==__LINE__)goto E;else if((line=__LINE__))switch(1) case 1:
+#define G2_YIELD														\
+	if(0){case __LINE__:;}												\
+	else																\
+		for(;;)															\
+			if(line==__LINE__)goto G2_COROUTINE_OUT;					\
+			else if((line=__LINE__)) switch(1)case 1:
+
