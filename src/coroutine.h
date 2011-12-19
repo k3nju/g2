501b7fd9ac90d413ff920e40d96b3768fb3081c2
planet <priv.planet@gmail.com>
2011-12-20 03:23:39 +0900

    Experimental coroutine added

--- src/coroutine.h
+++ src/coroutine.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #define G2_COROUTINE(l)							\
-	int *__FILE__##__FILE__ = l;				\
-	switch( *__FILE__##__FILE__ )				\
+	int *__FILE__##__LINE__ = l;				\
+	switch( *__FILE__##__LINE__ )				\
 	  case -1:									\
 		if(1)									\
 			{;G2_COROUTINE_OUT:break;}			\
@@ -12,5 +12,22 @@
 	if(0){case __LINE__:;}												\
 	else																\
 		for(;;)															\
-			if(*__FILE__##__FILE__==__LINE__)goto G2_COROUTINE_OUT;		\
-			else if((*__FILE__##__FILE__=__LINE__)) switch(1)case 1:
+			if(*__FILE__##__LINE__==__LINE__)goto G2_COROUTINE_OUT;		\
+			else if((*__FILE__##__LINE__=__LINE__)) switch(1)case 1:
+
+
+#define G2_COROUTINE_TSL							\
+	static __thread int __FILE__##__LINE__ = 0;		\
+	switch( __FILE__##__LINE__ )					\
+	  case -1:										\
+		if(1)										\
+			{;G2_COROUTINE_OUT:break;}				\
+		else case 0:
+
+#define G2_YIELD_TSL													\
+	if(0){case __LINE__:;}												\
+	else																\
+		for(;;)															\
+			if(__FILE__##__LINE__==__LINE__)goto G2_COROUTINE_OUT;		\
+			else if((__FILE__##__LINE__=__LINE__)) switch(1)case 1:
+
