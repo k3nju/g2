a3ce476955a60c01b76a2052567677ab5dd346d7
planet <priv.planet@gmail.com>
2011-12-20 04:52:33 +0900

    None

--- src/coroutine.h
+++ src/coroutine.h
@@ -22,7 +22,7 @@
 			if((*__FILE__##__LINE__=__LINE__)) switch(1)case 1:
 
 
-#define G2_COROUTINE_TLS							\
+#define G2_TLS_COROUTINE							\
 	static __thread int __FILE__##__LINE__ = 0;		\
 	switch( __FILE__##__LINE__ )					\
 	  case -1:										\
@@ -30,14 +30,14 @@
 			{;G2_COROUTINE_OUT:break;}				\
 		else case 0:
 
-#define G2_YIELD_ONCE_TLS												\
+#define G2_TLS_YIELD_ONCE												\
 	if(0){case __LINE__:;}												\
 	else																\
 		for(;;)															\
 			if(__FILE__##__LINE__==__LINE__)goto G2_COROUTINE_OUT;		\
 			else if((__FILE__##__LINE__=__LINE__)) switch(1)case 1:
 
-#define G2_YIELD_TLS													\
+#define G2_TLS_YIELD													\
 	if(0){case __LINE__:;}												\
 	else																\
 		for(;;)															\
